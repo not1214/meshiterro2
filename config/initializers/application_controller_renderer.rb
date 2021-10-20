0d662b5b9fe5a45be71da1f8b29840bd3f0d2896
EC2 Default User <ec2-user@ip-172-31-18-244.ec2.internal>
2021-10-20 09:16:01 +0000

    Finish 8章 復習

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'bf0dac7599a75cd0b022d8f96038a526c1fcc692eb67a1f0aee1032cdf82fb57350b0e608f7a16d166d7bb016dc3ea38cbb738021b9a342b2288cbec35fead6e'
